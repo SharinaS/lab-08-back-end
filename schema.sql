1026983e20dd314a0b3586104b58ced80c3d3895
Sharina Stubbs <sharinastubbs@gmail.com>
2019-08-23 07:46:01 -0700

    Updated Eventbrite with database functionality, but it's not fully working yet

--- schema.sql
+++ schema.sql
@@ -13,7 +13,6 @@ DROP TABLE IF EXISTS weather;
 
 CREATE TABLE weather (
    id SERIAL PRIMARY KEY,
-   /* get data from constructor function */
    search_query VARCHAR(255),
    forecast VARCHAR(255),
    time VARCHAR(255)
@@ -24,6 +23,8 @@ DROP TABLE IF EXISTS events;
 CREATE TABLE events (
    id SERIAL PRIMARY KEY,
    name VARCHAR(255),
+   search_query VARCHAR(255),
+   link VARCHAR(255)
    event_date VARCHAR(255),
    summary TEXT
    );
